37969912deebdac833d608663fbfa6db9bfce7bf
jayleenj <juarezjayleen15@gmail.com>
2026-06-11 10:04:27 -0400

    First js commit

--- variables.js
+++ variables.js
@@ -2,10 +2,10 @@
 let favoriteColor;
 
 // 1. Create a variable called `favoriteFood`.
-
+let favoriteFood;
 
 // 2. Create a variable called `favoriteNumber`.
-
+let favoriteNumber;
 
 
 
@@ -14,5 +14,12 @@ let favoriteColor;
 // - Use a string value of your favorite color.
 // - Use a string value of your favorite food.
 // - Use a number value of your favorite number.
+favoriteColor = "blue";
+favoriteFood = "yaroa";
+favoriteNumber = 15;
 
 // 4. Console log each variable on a new line.
+console.log(favoriteColor);
+console.log(favoriteFood);
+console.log(favoriteNumber);
+
